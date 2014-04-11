6857091a8d5b63158fce93ebd6762678da6b0632
Christian Perfect <christianperfect@gmail.com>
2014-04-11 12:07:46 +0100

    implement `curl`
    fixes #7

--- ctop.js
+++ ctop.js
@@ -673,6 +673,13 @@ CToP.applyTokens = {
 	"lambda": CToP.bind('\u03BB','.'),
 
 }
+CToP.applyTokens['curl'] = function(parentNode,contentMMLNode,firstArg,args,bvars,qualifiers,precedence) {
+	var mrow = CToP.createElement('mrow');
+	CToP.appendToken(mrow,'mo','\u2207');
+	CToP.appendToken(mrow,'mo','\u00d7');
+	CToP.applyTransform(mrow,args[0],precedence);
+	parentNode.appendChild(mrow);
+};
 CToP.applyTokens['not'] = function(parentNode,contentMMLNode,firstArg,args,bvars,qualifiers,precedence) {
 	var mrow = CToP.createElement('mrow');
 	CToP.appendToken(mrow,'mo','\u00ac');
